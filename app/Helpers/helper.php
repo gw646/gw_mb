5f27de8ec2f201210565c2398fdd6dc615412aa8
mdashikulilslam <ashik.nwu@gmail.com>
2024-03-14 04:05:09 +0600

    update sidebar

---
+++ app/Helpers/helper.php
@@ -0,0 +1,27 @@
+<?php
+use Spatie\Permission\Models\Permission;
+function getAdminPermissionGroupBy(){
+    $permissionGroup  = Permission::select('group_name')
+        ->orderBy('created_at','ASC')
+        ->groupBy('group_name')
+        ->get();
+    return $permissionGroup;
+}
+function getRoleDropdown($selected = 0){
+    $roles = \Spatie\Permission\Models\Role::get();
+    $html = '<option value="">Select Role</option>';
+    if (!empty($roles)){
+        foreach ($roles as $role){
+            $html .='<option value="'.$role->id.'"';
+            if ($role->id == $selected){
+                $html .=' selected ';
+            }
+            $html .=' >'.ucfirst($role->name).'</option>';
+        }
+    }
+    return $html;
+}
+function getUserRoleName()
+{
+    return auth()->user()->roles[0]->name;
+}

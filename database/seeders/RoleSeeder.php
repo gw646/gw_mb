4c546283b275c987a28093d33a70f9cc769752ef
mdashikulilslam <ashik.nwu@gmail.com>
2024-03-14 02:12:37 +0600

    auth add

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+
+class RoleSeeder extends Seeder
+{
+    public function run(): void
+    {
+        Role::firstOrCreate(['name' => ADMIN,'is_default' => 1]);
+    }
+}
--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -17,70 +17,17 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-xxl-4 col-lg-5">
-                <div class="card">
-                    <div class="card-header text-center bg-primary">
-                        <a href="{{route('lading')}}">
-                            <span><img src="{{asset('assets/images/mbttlogo.png')}}" alt=""  style="border-radius: 50%;max-height: 120px"></span>
-                            <h2 style="color: #fff">{{env('APP_NAME')}}</h2>
-                        </a>
-                    </div>
-                    <div class="card-body p-4">
-                        <div class="text-center w-75 m-auto">
-                            <h4 class="text-dark-50 text-center pb-0 fw-bold">Sign In</h4>
-                            <p class="text-muted mb-4">Enter your email address and password to access admin panel.</p>
-                        </div>
-
-                        <form action="#">
-
-                            <div class="mb-3">
-                                <label for="emailaddress" class="form-label">Email address</label>
-                                <input class="form-control" type="email" id="emailaddress" required="" placeholder="Enter your email">
-                            </div>
-
-                            <div class="mb-3">
-                                <a href="pages-recoverpw.html" class="text-muted float-end"><small>Forgot your password?</small></a>
-                                <label for="password" class="form-label">Password</label>
-                                <div class="input-group input-group-merge">
-                                    <input type="password" id="password" class="form-control" placeholder="Enter your password">
-                                    <div class="input-group-text" data-password="false">
-                                        <span class="password-eye"></span>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="mb-3 mb-3">
-                                <div class="form-check">
-                                    <input type="checkbox" class="form-check-input" id="checkbox-signin" checked>
-                                    <label class="form-check-label" for="checkbox-signin">Remember me</label>
-                                </div>
-                            </div>
-
-                            <div class="mb-3 mb-0 text-center">
-                                <button class="btn btn-primary" type="submit"> Log In </button>
-                            </div>
-
-                        </form>
-                    </div> <!-- end card-body -->
-                </div>
-                <!-- end card -->
-
-                <div class="row mt-3">
-                    <div class="col-12 text-center">
-                        <p class="text-muted">Don't have an account? <a href="pages-register.html" class="text-muted ms-1"><b>Sign Up</b></a></p>
-                    </div> <!-- end col -->
-                </div>
-                <!-- end row -->
-
-            </div> <!-- end col -->
+                @section('content')
+                @show
+            </div>
         </div>
-        <!-- end row -->
     </div>
     <!-- end container -->
 </div>
 <!-- end page -->
 
 <footer class="footer footer-alt">
-    2018 - <script>document.write(new Date().getFullYear())</script> © Hyper - Coderthemes.com
+    <script>document.write(new Date().getFullYear())</script> © {{env('APP_NAME')}}
 </footer>
 
 <!-- bundle -->

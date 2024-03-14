24c0f62d188e997cb61754db2efd4df7cacd0b80
mdashikulilslam <ashik.nwu@gmail.com>
2024-03-14 06:02:38 +0600

    update auth

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -7,6 +7,7 @@
     <link rel="shortcut icon" href="assets/images/favicon.ico">
     <link href="{{asset('assets/css/icons.min.css')}}" rel="stylesheet" type="text/css"/>
     <link href="{{asset('assets/css/app.min.css')}}" rel="stylesheet" type="text/css" id="app-style"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 </head>
 
 <body class="loading" data-layout-color="light" data-leftbar-theme="dark" data-layout-mode="fluid"

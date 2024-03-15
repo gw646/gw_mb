4097193e356e6193ec0a2653d2b1185eabc029af
mdashikulilslam <ashik.nwu@gmail.com>
2024-03-15 17:00:13 +0600

    update

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,11 +3,20 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('admin.dashboard');
+        $registrants = User::with('roles')
+            ->whereHas('roles',function ($q){
+                $q->where('name',REGISTRANTS);
+            })->orderBy('created_at')->get();
+
+        return view('admin.dashboard')
+            ->with([
+                'registrants'=>$registrants
+            ]);
     }
 }
